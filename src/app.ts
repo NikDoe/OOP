b9b8f3fbaef374cc577fba9cdc315d4daee07cde
NikDoe <nikd0e@yandex.ru>
2021-11-22 17:00:50 +0300

    feature(OOP): creating a class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-// Code goes here!
+class Department {
+    name: string;
+    constructor(n: string) {
+        this.name = n;
+    }
+}
+
+const it = new Department('IT');
+console.log(it);
+
+//////////short version
+
+// class Department {
+//     constructor(public name: string) {}
+// }
+//
+// const it = new Department('IT_DEV');
+// console.log(it);
